a574fb7db9f496044bc3412482a798b59e50c04a
polash016 <robin.shrkr@gmail.com>
2023-05-04 01:06:37 +0600

    chef data added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const data = require('./data/data.json');
 
 app.get('/', (req, res) => {
     res.send("Server Is Running")
+});
+app.get('/chef', (req, res) => {
+    res.send(data)
 })
 app.listen(port, ()=>{
     console.log(`Server Running on Port: ${port}`)
